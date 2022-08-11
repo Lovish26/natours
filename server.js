08ec3bcbe987ffaaaaf88f2a9a5faee4bf814806
Lovish Kamboj <coc2610199914@gmail.com>
2022-08-11 11:34:50 +0530

    Implemented cors

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on  http://localhost:${port}...`);
+  console.log(`App running on ${port}...`);
 });
 
 process.on('unhandledRejection', (err) => {
