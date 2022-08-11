ffbd7b953f8f075c4dc6093f1f4e6686c71c27a4
Lovish Kamboj <coc2610199914@gmail.com>
2022-08-11 13:28:18 +0530

    Implemented stripe messages

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -6,9 +6,13 @@ const hideAlert = () => {
 };
 
 // type is 'success' or 'error'
-const showAlert = (type, msg) => {
+const showAlert = (type, msg, time = 7) => {
   hideAlert();
   const markup = `<div class="alert alert--${type}">${msg}</div>`;
   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-  window.setTimeout(hideAlert, 5000);
+  window.setTimeout(hideAlert, time * 1000);
 };
+
+// This is implemented for alerts that comes from query
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20), alertMessage;
