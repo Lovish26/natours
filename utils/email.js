4be7a2ec16f36658a9c71189712536571619c5c6
Lovish Kamboj <coc2610199914@gmail.com>
2022-08-12 11:48:30 +0530

    Fixed mailtrap in prod

--- utils/email.js
+++ utils/email.js
@@ -11,10 +11,10 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      //sendgrid
-      return 1;
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   //sendgrid
+    //   return 1;
+    // }
 
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
